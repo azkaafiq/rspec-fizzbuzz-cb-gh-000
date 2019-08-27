ccb56d3680a9094ba43cc0dc379436cf9561e4b2
AHMAD AZKA AFIQ AHMAD KAMRAN <azkarucoo@hotmail.com>
2019-08-27 15:12:51 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,11 +18,14 @@ describe "fizzbuzz" do
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
     fizz_15 = fizzbuzz(15)
+    fizz_15 = fizzbuzz(30)
+    fizz_15 = fizzbuzz(450)
 
     expect(fizz_15).to eq("FizzBuzz")
   end
   it 'returns nil when the number is not divisible by 3 or 5' do
     fizz_4 = fizzbuzz(4)
+    fizz_4 = fizzbuzz(14)
 
     expect(fizz_4).to eq(nil)
   end
