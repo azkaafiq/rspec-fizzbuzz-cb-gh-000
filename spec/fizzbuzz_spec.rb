ef2abffd2720a4d6db1240c04f7b891f8b1afa1c
AHMAD AZKA AFIQ AHMAD KAMRAN <azkarucoo@hotmail.com>
2019-08-27 15:12:11 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,11 +4,15 @@ require_relative './spec_helper.rb'
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
+    fizz_3 = fizzbuzz(6)
+    fizz_3 = fizzbuzz(999)
 
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
+    fizz_5 = fizzbuzz(10)
+    fizz_5 = fizzbuzz(125)
 
     expect(fizz_5).to eq("Buzz")
   end
